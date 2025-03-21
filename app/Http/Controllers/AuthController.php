6c1825c082ca494f6ce3b60b4d3c6d3833ccb319
Hussein-02 <hussein02ab@gmail.com>
2025-03-21 14:22:07 +0200

    finished login and user details

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -26,4 +27,25 @@ class AuthController extends Controller
 
         return response()->json(['user' => $user, 'token' => $token], 201);
     }
+
+    public function login(Request $request){
+        $credentials = $request->only('email','password');
+
+        if(!$token = JWTAuth::attempt($credentials)){
+            return response()->json(['error'=>'Unauthorized'],401);
+        }
+
+        return response()->json(['token'=>$token]);
+    }
+
+    //to fetch user details
+    public function me(){
+        return response()->json(Auth::user());
+    }
+
+
+
+
+
+
 }
