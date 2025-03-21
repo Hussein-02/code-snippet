9acb9f8563d21a57371abd3041d445428dc87b44
Hussein-02 <hussein02ab@gmail.com>
2025-03-22 01:30:10 +0200

    finished factory

--- database/factories/SnippetFactory.php
+++ database/factories/SnippetFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Snippet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +16,18 @@ class SnippetFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    protected $model = Snippet::class;
+    
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'code' => $this->faker->text(200),
+            'language' => $this->faker->randomElement(['PHP', 'JavaScript', 'Python', 'C++']),
+            'description' => $this->faker->sentence(10),
+            'is_favorite' => $this->faker->boolean(),
+            'user_id' => User::factory(),
         ];
     }
 }
