7da74e85b03f82c4de2ec148c727cb630d26fc92
Hussein-02 <hussein02ab@gmail.com>
2025-03-22 16:09:22 +0200

    added signup and login

---
+++ Client/src/pages/Login/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import getBaseURL from "../../utils/baseURL";
+import "./Login.css";
+
+const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(`${getBaseURL()}/login`, form, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+
+      if (response.data.success) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+
+        navigate("/home");
+      }
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  };
+
+  return (
+    <div className="body">
+      <h1 className="logo">CODE SNIPPET</h1>
+      {/* <img src="/logo.svg" alt="" /> */}
+      <div className="login-section">
+        <h1>Log In</h1>
+        <form className="login-form" id="loginForm" onSubmit={handleSubmit}>
+          <div className="login-input">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              // value={email}
+              onChange={(e) => {
+                setForm({
+                  ...form,
+                  email: e.target.value,
+                });
+              }}
+            />
+          </div>
+
+          <div className="login-input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              // value={password}
+              onChange={(e) => {
+                setForm({
+                  ...form,
+                  password: e.target.value,
+                });
+              }}
+            />
+          </div>
+
+          <input type="submit" value="Log In" className="login-btn" />
+        </form>
+        <a href="./signup">
+          <button className="signup-btn">Sign Up</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- Client/src/routers/router.jsx
+++ Client/src/routers/router.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-// import Login from "../pages/Login";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      //   {
-      //     path: "/",
-      //     element: <Login />,
-      //   },
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
     ],
   },
 ]);
