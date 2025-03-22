cfb031902836260f96791a49311d7a2ec470bb4f
Hussein-02 <hussein02ab@gmail.com>
2025-03-23 01:19:07 +0200

    filter works on backend

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 // import Navbar from "../components/Navbar";
 import axios from "axios";
@@ -32,6 +33,26 @@ const Home = () => {
     }
   };
 
+  const filterSnippets = async (token) => {
+    try {
+      const response = await axios.get(`${getBaseURL()}/snippets?search=${searchQuery}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.data.status) {
+        setSnippets(response.data.snippets);
+        setLoading(false);
+      } else {
+        console.error("Failed to fetch snippets");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Error fetching snippets:", error);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -40,6 +61,15 @@ const Home = () => {
 
     fetchSnippets(token);
   }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    }
+
+    filterSnippets(token);
+  }, [searchQuery]);
 
   if (loading) {
     return <div>Loading snippets...</div>;
@@ -64,10 +94,7 @@ const Home = () => {
       <div id="photo-cards" className="cards-container">
         {snippets.length > 0 ? (
           snippets.map((snippet) => (
-            <div
-              key={snippet.id}
-              style={{ border: "1px solid #ddd", margin: "10px", padding: "10px" }}
-            >
+            <div key={snippet.id}>
               <h3>{snippet.title}</h3>
               <p>
                 <strong>Language:</strong> {snippet.language}
