9bf2d9cfa15a0124559d10b8b26d8b006eb3566b
Hussein-02 <hussein02ab@gmail.com>
2025-03-22 13:21:31 +0200

    tested all apis and all work

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SnippetController;
 
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     // Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::apiResource('snippets',SnippetController::class);
-    Route::patch('snippets/{snippet}/favorite',[SnippetController::class,'toggleFavorite']);
+    Route::apiResource('snippets', SnippetController::class);
+    Route::patch('snippets/{snippet}/favorite', [SnippetController::class, 'toggleFavorite']);
 });
-
